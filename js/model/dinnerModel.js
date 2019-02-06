ebf453e752c987e5a8b9121923e94b7f2b644526
Matt Bergstrom <matt_bergstrom@icloud.com>
2019-02-06 08:55:08 +0100

    DONE WITH LAB :FIRE:

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -2,6 +2,8 @@
 var DinnerModel = function() {
 
   const API_KEY = "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
+  var observers=[];
+  var selectedDishes=[];
 
   this.errorMsg = function (error){
     console.error("Error: ", error);
